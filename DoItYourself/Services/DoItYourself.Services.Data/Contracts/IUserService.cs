688e5bfcef96403dfebb0e485b8650e6e2204148
dimitarsd <dsd363978@yahoo.com>
2016-02-17 00:11:23 +0200

    Added services for User

--- DoItYourself/Services/DoItYourself.Services.Data/Contracts/IUserService.cs
+++ DoItYourself/Services/DoItYourself.Services.Data/Contracts/IUserService.cs
@@ -1,6 +1,19 @@
 ﻿namespace DoItYourself.Services.Data.Contracts
 {
+    using System.Linq;
+
+    using DoItYourself.Data.Models;
+
     public interface IUserService
     {
+        IQueryable<User> AllUsers();
+
+        IQueryable<User> AllUsersWithDeleted();
+
+        User GetById(object id);
+
+        void DeleteUser(User user);
+
+        void HardDeleteUser(User user);
     }
 }
--- DoItYourself/Services/DoItYourself.Services.Data/UserService.cs
+++ DoItYourself/Services/DoItYourself.Services.Data/UserService.cs
@@ -1,5 +1,7 @@
 ﻿namespace DoItYourself.Services.Data
 {
+    using System.Linq;
+
     using Contracts;
     using DoItYourself.Data.Common;
     using DoItYourself.Data.Models;
@@ -7,5 +9,35 @@
     public class UserService : IUserService
     {
         private readonly IDbRepository<User> users;
+
+        public UserService(IDbRepository<User> users)
+        {
+            this.users = users;
+        }
+
+        public IQueryable<User> AllUsers()
+        {
+            return this.users.All();
+        }
+
+        public IQueryable<User> AllUsersWithDeleted()
+        {
+            return this.users.AllWithDeleted();
+        }
+
+        public User GetById(object id)
+        {
+            return this.users.GetById(id);
+        }
+
+        public void DeleteUser(User user)
+        {
+            this.users.Delete(user);
+        }
+
+        public void HardDeleteUser(User user)
+        {
+            this.users.HardDelete(user);
+        }
     }
 }
