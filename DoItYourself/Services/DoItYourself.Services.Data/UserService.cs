01eb428b63e560b261d3cc86446b4e24a5940632
dimitarsd <dsd363978@yahoo.com>
2016-02-16 22:54:05 +0200

    Fixed bug - User to work with IDbRepository

--- DoItYourself/Services/DoItYourself.Services.Data/UserService.cs
+++ DoItYourself/Services/DoItYourself.Services.Data/UserService.cs
@@ -1,8 +1,11 @@
 ﻿namespace DoItYourself.Services.Data
 {
     using Contracts;
+    using DoItYourself.Data.Common;
+    using DoItYourself.Data.Models;
 
     public class UserService : IUserService
     {
+        private readonly IDbRepository<User> users;
     }
 }
