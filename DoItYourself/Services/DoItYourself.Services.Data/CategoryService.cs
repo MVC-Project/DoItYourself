9a35e5e76067538524aacd877dab481d1f712e0d
dimitarsd <dsd363978@yahoo.com>
2016-02-18 00:25:58 +0200

    Added services for Category and Country

--- DoItYourself/Services/DoItYourself.Services.Data/CategoryService.cs
+++ DoItYourself/Services/DoItYourself.Services.Data/CategoryService.cs
@@ -1,8 +1,65 @@
 ﻿namespace DoItYourself.Services.Data
 {
+    using System.Linq;
+
     using Contracts;
+    using DoItYourself.Data.Common;
+    using DoItYourself.Data.Models;
 
     public class CategoryService : ICategoryService
     {
+        private readonly IDbRepository<Category> categories;
+
+        public CategoryService(IDbRepository<Category> categories)
+        {
+            this.categories = categories;
+        }
+
+        public IQueryable<Category> AllCategories()
+        {
+            return this.categories.All();
+        }
+
+        public IQueryable<Category> AllCategoriesWithDeleted()
+        {
+            return this.categories.AllWithDeleted();
+        }
+
+        public Category GetById(int id)
+        {
+            return this.categories.GetById(id);
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            this.categories.Delete(category);
+            this.categories.Save();
+        }
+
+        public void HardDeleteCategory(Category category)
+        {
+            this.categories.HardDelete(category);
+            this.categories.Save();
+        }
+
+        public void DeleteCategoryById(int id)
+        {
+            var category = this.categories.GetById(id);
+            this.categories.Delete(category);
+            this.categories.Save();
+        }
+
+        public void HardDeleteCategoryById(int id)
+        {
+            var category = this.categories.GetById(id);
+            this.categories.HardDelete(category);
+            this.categories.Save();
+        }
+
+        public void AddCategory(Category category)
+        {
+            this.categories.Add(category);
+            this.categories.Save();
+        }
     }
 }
--- DoItYourself/Services/DoItYourself.Services.Data/CountryService.cs
+++ DoItYourself/Services/DoItYourself.Services.Data/CountryService.cs
@@ -1,8 +1,97 @@
 ﻿namespace DoItYourself.Services.Data
 {
+    using System.Linq;
+
     using Contracts;
+    using DoItYourself.Data.Common;
+    using DoItYourself.Data.Models;
 
     public class CountryService : ICountryService
     {
+        private readonly IDbRepository<Country> countries;
+
+        public CountryService(IDbRepository<Country> countries)
+        {
+            this.countries = countries;
+        }
+
+        public IQueryable<Country> AllCountries()
+        {
+            return this.countries.All()
+                .OrderBy(country => country.Name);
+        }
+
+        public IQueryable<Country> AllCountriesWithDeleted()
+        {
+            return this.countries.AllWithDeleted()
+                .OrderBy(country => country.Name);
+        }
+
+        public IQueryable<string> AllCountriesNames()
+        {
+            return this.countries.All()
+                .OrderBy(country => country.Name)
+                .Select(country => country.Name);
+        }
+
+        public IQueryable<string> AllCountriesAbbreviations()
+        {
+            return this.countries.All()
+                .OrderBy(country => country.Abbreviation)
+                .Select(country => country.Abbreviation);
+        }
+
+        public Country GetById(int id)
+        {
+            return this.countries.GetById(id);
+        }
+
+        public string GetCountryNameByAbbreviation(string abbreviation)
+        {
+            return this.countries.All()
+                .Where(country => country.Abbreviation == abbreviation)
+                .Select(country => country.Name)
+                .ToString();
+        }
+
+        public string GetCountryAbbreviationByName(string name)
+        {
+            return this.countries.All()
+                .Where(country => country.Name == name)
+                .Select(country => country.Abbreviation)
+                .ToString();
+        }
+
+        public void DeleteCountry(Country country)
+        {
+            this.countries.Delete(country);
+            this.countries.Save();
+        }
+
+        public void HardDeleteCountry(Country country)
+        {
+            this.countries.HardDelete(country);
+            this.countries.Save();
+        }
+
+        public void DeleteCountryById(int id)
+        {
+            var country = this.countries.GetById(id);
+            this.countries.Delete(country);
+            this.countries.Save();
+        }
+
+        public void HardDeleteCountryById(int id)
+        {
+            var country = this.countries.GetById(id);
+            this.countries.HardDelete(country);
+            this.countries.Save();
+        }
+
+        public void AddCountry(Country country)
+        {
+            this.countries.Add(country);
+            this.countries.Save();
+        }
     }
 }
