673b45246ce7df22f914c52aa0a75a62c92b91db
dimitarsd <dsd363978@yahoo.com>
2016-02-18 01:48:51 +0200

    Added services for Image and Location

--- DoItYourself/Services/DoItYourself.Services.Data/Contracts/ILocationService.cs
+++ DoItYourself/Services/DoItYourself.Services.Data/Contracts/ILocationService.cs
@@ -1,6 +1,27 @@
 ﻿namespace DoItYourself.Services.Data.Contracts
 {
+    using System.Linq;
+
+    using DoItYourself.Data.Models;
+
     public interface ILocationService
     {
+        IQueryable<Location> AllLocations();
+
+        IQueryable<Location> AllLocationsWithDeleted();
+
+        Location GetById(int id);
+
+        IQueryable<string> AllCities();
+
+        void DeleteLocation(Location location);
+
+        void HardDeleteLocation(Location location);
+
+        void DeleteLocationById(int id);
+
+        void HardDeleteLocationById(int id);
+
+        void AddLocation(Location location);
     }
 }
--- DoItYourself/Services/DoItYourself.Services.Data/LocationService.cs
+++ DoItYourself/Services/DoItYourself.Services.Data/LocationService.cs
@@ -1,8 +1,74 @@
 ﻿namespace DoItYourself.Services.Data
 {
+    using System.Linq;
+
     using Contracts;
+    using DoItYourself.Data.Common;
+    using DoItYourself.Data.Models;
 
     public class LocationService : ILocationService
     {
+        private readonly IDbRepository<Location> locations;
+
+        public LocationService(IDbRepository<Location> locations)
+        {
+            this.locations = locations;
+        }
+
+        public IQueryable<Location> AllLocations()
+        {
+            return this.locations.All()
+                .OrderBy(location => location.City);
+        }
+
+        public IQueryable<Location> AllLocationsWithDeleted()
+        {
+            return this.locations.AllWithDeleted()
+                .OrderBy(location => location.City);
+        }
+
+        public Location GetById(int id)
+        {
+            return this.locations.GetById(id);
+        }
+
+        public IQueryable<string> AllCities()
+        {
+            return this.locations.All()
+                .OrderBy(location => location.City)
+                .Select(location => location.City);
+        }
+
+        public void DeleteLocation(Location location)
+        {
+            this.locations.Delete(location);
+            this.locations.Save();
+        }
+
+        public void HardDeleteLocation(Location location)
+        {
+            this.locations.HardDelete(location);
+            this.locations.Save();
+        }
+
+        public void DeleteLocationById(int id)
+        {
+            var location = this.locations.GetById(id);
+            this.locations.Delete(location);
+            this.locations.Save();
+        }
+
+        public void HardDeleteLocationById(int id)
+        {
+            var location = this.locations.GetById(id);
+            this.locations.Delete(location);
+            this.locations.Save();
+        }
+
+        public void AddLocation(Location location)
+        {
+            this.locations.Add(location);
+            this.locations.Save();
+        }
     }
 }
