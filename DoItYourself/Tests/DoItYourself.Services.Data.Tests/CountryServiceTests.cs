d122ddd86a6ace0ef86ef24517a0d2fdc0f8d1a6
dimitarsd <dsd363978@yahoo.com>
2016-02-23 10:47:48 +0200

    Added unit tests for services Country Image, Location and ProjectComment

---
+++ DoItYourself/Tests/DoItYourself.Services.Data.Tests/CountryServiceTests.cs
@@ -0,0 +1,55 @@
+﻿namespace DoItYourself.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Contracts;
+    using DoItYourself.Data.Models;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class CountryServiceTests
+    {
+        private static readonly IQueryable<Country> mockedCountries = new List<Country>().AsQueryable();
+
+        private static Mock<ICountryService> mockedCountriesData;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            var mockedCountriesService = new Mock<ICountryService>();
+            mockedCountriesService.Setup(s => s.AllCountries())
+                .Returns(mockedCountries);
+
+            mockedCountriesService.Setup(s => s.AddCountry(It.IsAny<Country>()))
+                .Verifiable();
+
+            mockedCountriesData = mockedCountriesService;
+        }
+
+        [TestMethod]
+        public void GetAllCountriesShouldNotReturnNull()
+        {
+            IQueryable<Country> countries = mockedCountriesData.Object.AllCountries();
+            Assert.AreNotEqual(null, countries);
+        }
+
+        [TestMethod]
+        public void GetAllCountriesWithDeletedShouldNotReturnNull()
+        {
+            IQueryable<Country> countries = mockedCountriesData.Object.AllCountriesWithDeleted();
+            Assert.AreNotEqual(null, countries);
+        }
+
+        [TestMethod]
+        public void AddCountryShouldBeCalled()
+        {
+            mockedCountriesData.Object.AddCountry(new Country());
+            mockedCountriesData.Verify(s => s.AddCountry(It.IsAny<Country>()));
+        }
+    }
+}
---
+++ DoItYourself/Tests/DoItYourself.Services.Data.Tests/ImageServiceTests.cs
@@ -0,0 +1,55 @@
+﻿namespace DoItYourself.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Contracts;
+    using DoItYourself.Data.Models;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class ImageServiceTests
+    {
+        private static readonly IQueryable<Image> mockedImages= new List<Image>().AsQueryable();
+
+        private static Mock<IImageService> mockedImagesData;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            var mockedImagesService = new Mock<IImageService>();
+            mockedImagesService.Setup(s => s.AllImages())
+                .Returns(mockedImages);
+
+            mockedImagesService.Setup(s => s.AddImage(It.IsAny<Image>()))
+                .Verifiable();
+
+            mockedImagesData = mockedImagesService;
+        }
+
+        [TestMethod]
+        public void GetAllImagesShouldNotReturnNull()
+        {
+            IQueryable<Image> images = mockedImagesData.Object.AllImages();
+            Assert.AreNotEqual(null, images);
+        }
+
+        [TestMethod]
+        public void GetAllImagesWithDeletedShouldNotReturnNull()
+        {
+            IQueryable<Image> images = mockedImagesData.Object.AllImagesWithDeleted();
+            Assert.AreNotEqual(null, images);
+        }
+
+        [TestMethod]
+        public void AddImageShouldBeCalled()
+        {
+            mockedImagesData.Object.AddImage(new Image());
+            mockedImagesData.Verify(s => s.AddImage(It.IsAny<Image>()));
+        }
+    }
+}
