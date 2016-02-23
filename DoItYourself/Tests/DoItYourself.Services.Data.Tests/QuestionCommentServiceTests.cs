f67f712420065f5200b188b4b2a734ef34ef6f2f
dimitarsd <dsd363978@yahoo.com>
2016-02-23 11:26:05 +0200

    Added unit tests for services Rating, User, Video and QuestionComment

---
+++ DoItYourself/Tests/DoItYourself.Services.Data.Tests/QuestionCommentServiceTests.cs
@@ -0,0 +1,55 @@
+﻿namespace DoItYourself.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Contracts;
+    using DoItYourself.Data.Models;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class QuestionCommentServiceTests
+    {
+        private static readonly IQueryable<QuestionComment> mockedQuestionComments = new List<QuestionComment>().AsQueryable();
+
+        private static Mock<IQuestionCommentService> mockedQuestionCommentsData;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            var mockedQuestionCommentsService = new Mock<IQuestionCommentService>();
+            mockedQuestionCommentsService.Setup(s => s.AllQuestionComments())
+                .Returns(mockedQuestionComments);
+
+            mockedQuestionCommentsService.Setup(s => s.AddQuestionComment(It.IsAny<QuestionComment>()))
+                .Verifiable();
+
+            mockedQuestionCommentsData = mockedQuestionCommentsService;
+        }
+
+        [TestMethod]
+        public void GetAllQuestionCommentsShouldNotReturnNull()
+        {
+            IQueryable<QuestionComment> questionComments = mockedQuestionCommentsData.Object.AllQuestionComments();
+            Assert.AreNotEqual(null, questionComments);
+        }
+
+        [TestMethod]
+        public void GetAllQuestionsWithDeletedShouldNotReturnNull()
+        {
+            IQueryable<QuestionComment> questionComments = mockedQuestionCommentsData.Object.AllQuestionCommentsWithDeleted();
+            Assert.AreNotEqual(null, questionComments);
+        }
+
+        [TestMethod]
+        public void AddQuestionCommentShouldBeCalled()
+        {
+            mockedQuestionCommentsData.Object.AddQuestionComment(new QuestionComment());
+            mockedQuestionCommentsData.Verify(s => s.AddQuestionComment(It.IsAny<QuestionComment>()));
+        }
+    }
+}
---
+++ DoItYourself/Tests/DoItYourself.Services.Data.Tests/RatingServiceTests.cs
@@ -0,0 +1,55 @@
+﻿namespace DoItYourself.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Contracts;
+    using DoItYourself.Data.Models;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class RatingServiceTests
+    {
+        private static readonly IQueryable<Rating> mockedRatings = new List<Rating>().AsQueryable();
+
+        private static Mock<IRatingService> mockedRatingsData;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            var mockedRatingsService = new Mock<IRatingService>();
+            mockedRatingsService.Setup(s => s.AllRatings())
+                .Returns(mockedRatings);
+
+            mockedRatingsService.Setup(s => s.AddRating(It.IsAny<Rating>()))
+                .Verifiable();
+
+            mockedRatingsData = mockedRatingsService;
+        }
+
+        [TestMethod]
+        public void GetAllRatingsShouldNotReturnNull()
+        {
+            IQueryable<Rating> ratings = mockedRatingsData.Object.AllRatings();
+            Assert.AreNotEqual(null, ratings);
+        }
+
+        [TestMethod]
+        public void GetAllRatingsWithDeletedShouldNotReturnNull()
+        {
+            IQueryable<Rating> ratings = mockedRatingsData.Object.AllRatingWithDeleted();
+            Assert.AreNotEqual(null, ratings);
+        }
+
+        [TestMethod]
+        public void AddRatingShouldBeCalled()
+        {
+            mockedRatingsData.Object.AddRating(new Rating());
+            mockedRatingsData.Verify(s => s.AddRating(It.IsAny<Rating>()));
+        }
+    }
+}
